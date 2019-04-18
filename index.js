c5598563ae67aa2feb0fa9ce5be67d9f4e257313
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-04-18 18:57:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
 function sayHiToGrandma(string){
   if(string === string.toUpperCase()){
     return "YES INDEED!";
-  }else if(string === string.toLowerCase()){
+  }else {
     return "I can't hear you!"
   }
 }
